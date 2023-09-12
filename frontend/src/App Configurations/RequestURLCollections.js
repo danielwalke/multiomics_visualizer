4fa6c2c51bd65b2feba2d9d967d7ecd52446ab04
danielwalke <daniel.walke@ovgu.de>
2023-09-12 10:24:20 +0200

    added isas logo for new server

--- frontend/src/App Configurations/RequestURLCollections.js
+++ frontend/src/App Configurations/RequestURLCollections.js
@@ -1,7 +1,7 @@
 import {host, portNumber} from "./SystemSettings";
 
-export const endpoint_uploadFile = host +"/heuristic/upload/reduceDimensionAndCluster/"
+export const endpoint_uploadFile = host +"/upload/reduceDimensionAndCluster/"
 
-export const endpoint_cluster = host +"/heuristic/upload/cluster/"
+export const endpoint_cluster = host +"/upload/cluster/"
 
-export const endpoint_KeggPathway = host + "/heuristic/upload/keggPathway/"
+export const endpoint_KeggPathway = host + "/upload/keggPathway/"
