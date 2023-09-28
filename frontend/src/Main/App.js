9f76c320925822b6239cfdcbd1f7382ff618adee
danielwalke <daniel.walke@ovgu.de>
2023-09-28 10:08:57 +0200

    matomo tracking

--- frontend/src/Main/App.js
+++ frontend/src/Main/App.js
@@ -6,6 +6,7 @@ import {ThemeProvider} from "@material-ui/styles";
 import {Provider} from "mobx-react";
 import DataStore from "../Components/State Management/DataStore";
 import ResponseStore from "../Components/State Management/ResponseStore";
+import { useEffect } from 'react';
 
 const theme = createMuiTheme({
     palette:{
@@ -16,6 +17,24 @@ const theme = createMuiTheme({
 })
 
 function App() {
+
+    useEffect(()=>{
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(["setCookieDomain", "*.multiomics-visualizer.isas.de"]);
+        _paq.push(["setDoNotTrack", true]);
+        _paq.push(["disableCookies"]);
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="https://matomo.isas.de/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '1']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+    }, [])
+
     return (
         <Provider DataStore={DataStore} ResponseStore={ResponseStore}>
             <div className="App" style={{position: "relative", overflow: "hidden"}}>
