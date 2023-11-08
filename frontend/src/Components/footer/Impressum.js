2a5d4849501420cf559a855cefcfb006ff912e30
danielwalke <daniel.walke@ovgu.de>
2023-11-08 14:45:55 +0100

    new site notice and font color to black

--- frontend/src/Components/footer/Impressum.js
+++ frontend/src/Components/footer/Impressum.js
@@ -3,32 +3,56 @@ import "./Glass.css"
 export const Impressum = () => {
     return(
         <div style={{height:"100%", overflowY:"auto", width:"100%", display:"flex", justifyContent:"center", padding:"1rem"}}>
-            <div style={{padding:".75rem", width: "80%", minHeight:"100%", textAlign:"justify"}} className={"glass"}>
+            <div style={{padding:".75rem", width: "80%", minHeight:"100%", textAlign:"justify", lineHeight:"2rem"}} className={"glass"}>
                 <h2>Impressum</h2>
                 <div>
 
-                    Die nachstehenden Informationen enthalten die gesetzlich vorgesehenen Pflichtangaben zur Anbieterkennzeichnung sowie wichtige rechtliche Hinweise zur Internetpräsenz des Leibniz-Instituts für Analytische Wissenschaften - ISAS - e.V. (www.isas.de).
+                    Die nachstehenden Informationen enthalten die gesetzlich vorgesehenen Pflichtangaben zur Anbieterkennzeichnung sowie wichtige rechtliche Hinweise zur Internetpräsenz des Multiomics Visualizer (<a href="https://multiomics-visualizer.isas.de/" target="_blank">https://multiomics-visualizer.isas.de/</a>).
 
                     <h3>Anbieter</h3>
                     Der Anbieter dieser Internetpräsenz im Rechtssinne ist das Leibniz-Institut für Analytische Wissenschaften - ISAS - e.V.
 
                     <h3>Adresse</h3>
-                    Leibniz-Institut für Analytische Wissenschaften - ISAS - e.V.<br/>
+                    <p style={{"userSelect": "all"}}>Leibniz-Institut für Analytische Wissenschaften - ISAS - e.V.<br/>
                     Bunsen-Kirchhoff-Str. 11<br/>
                     D-44139 Dortmund<br/>
+                    </p>
+                    T: <span style={{"userSelect": "all"}}>+49 (0)231 1392-0</span>
 
                     <h3>Rechtsträger</h3>
                     Leibniz-Institut für Analytische Wissenschaften - ISAS - e.V., Bunsen-Kirchhoff-Str. 11, 44139 Dortmund<br/>
+
+                    Registergericht: Amtsgericht Dortmund<br/>
+
+                    Registernummer: VR 1724<br/>
 
                     Umsatzsteuer-Identifikationsnummer gemäß § 27a Umsatzsteuergesetz: DE 124913007<br/>
 
                     <h3>Geschäftsführer</h3>
                     Das Leibniz-Institut für Analytische Wissenschaften - ISAS - e.V. wird gesetzlich vertreten durch zwei Mitglieder des Vorstands.<br/>
 
-                    <h3>Vorstand</h3>Prof. Dr. Albert Sickmann, Jürgen Bethke<br/>
+                    Vorstand: Prof. Dr. Albert Sickmann, Jürgen Bethke
+
+                    <h3>Wissenschaftliche Verantwortlichkeit</h3>
+
+                    Verantwortlich für die Inhalte auf dieser Website:<br/>
+
+                    Leibniz-Institut für Analytische Wissenschaften - ISAS - e.V.<br/>
+                    Nachwuchsgruppe Mehrdimensionale Omics-Datenanalyse<br/>
+                    Prof. Dr. Robert Heyer<br/>
+                    T: <span style={{"userSelect": "all"}}>+49 (0)231 1392-271</span><br/>
+                    F: <span style={{"userSelect": "all"}}>+49 (0)231 1392-200</span><br/>
+                    <a href="mailto:robert.heyer@isas.de">Robert Heyer</a>, <span style={{"userSelect": "all"}}>robert.heyer@isas.de</span><br/>
 
                     <h3>Rechtsform</h3>
                     Eingetragener Verein (e.V.)<br/>
+
+                    <h3>Rechtliche Hinweise zur Haftung/Disclaimer</h3>
+
+                    <p>Das Leibniz-Institut für Analytische Wissenschaften - ISAS - e.V. ist um Richtigkeit und Aktualität der auf dieser Internetpräsenz bereitgestellten Informationen bemüht. Trotzdem können Fehler und Unklarheiten nicht vollständig ausgeschlossen werden. Das Leibniz-Institut für Analytische Wissenschaften - ISAS - e.V. übernimmt deshalb keine Gewähr für die Aktualität, Richtigkeit, Vollständigkeit oder Qualität der bereitgestellten Informationen. Für Schäden materieller oder immaterieller Art, die durch die Nutzung oder Nichtnutzung der dargebotenen Informationen bzw. durch die Nutzung fehlerhafter und unvollständiger Informationen unmittelbar oder mittelbar verursacht werden, haftet das Leibniz-Institut für Analytische Wissenschaften - ISAS - e.V. nicht, sofern ihm nicht nachweislich vorsätzliches oder grob fahrlässiges Verschulden zur Last fällt. Gleiches gilt für kostenlos bereitgehaltene Software. Das Leibniz-Institut für Analytische Wissenschaften - ISAS - e.V. behält es sich vor, Teile des Internetangebots oder das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen. Die Verantwortlichkeit für fremde Inhalte, die beispielsweise durch direkte oder indirekte Verknüpfungen (z. B. sogenannte „Links“) zu anderen Anbietern bereitgehalten werden, setzt unter anderem die positive Kenntnis des rechtswidrigen bzw. strafbaren Inhaltes voraus. Fremde Inhalte sind in geeigneter Weise gekennzeichnet. Das Leibniz-Institut für Analytische Wissenschaften - ISAS - e.V. hat auf fremde Inhalte keinen Einfluss und macht sich diese Inhalte auch nicht zu Eigen. Das Leibniz-Institut für Analytische Wissenschaften - ISAS - e.V. hat keine Kenntnis über rechtswidrige oder anstößige Inhalte auf den mit seiner Internetpräsenz verknüpften Seiten fremder Anbieter. Sollten auf den verknüpften Seiten fremder Anbieter dennoch rechtswidrige oder anstößige Inhalte enthalten sein, so distanziert sich das Leibniz-Institut für Analytische Wissenschaften - ISAS - e.V. von diesen Inhalten ausdrücklich.</p>
+
+                    <h3>Rechtliche Hinweise zum Urheberrecht</h3>
+                    <p>Das Layout der Webseite, die verwendeten Grafiken sowie die sonstigen Inhalte der Internetpräsenz des Leibniz-Instituts für Analytische Wissenschaften - ISAS - e.V. sind urheberrechtlich geschützt. Es gelten die auf unseren Webseiten veröffentlichten <a href={"https://www.isas.de/nutzungsrechte-und-bedingungen#:~:text=Die%20Inhalte%20der%20Webseiten%20sind,des%20ISAS%20e.V.%20nicht%20gestattet."} target={"_blank"}>Nutzungsrechte und -bedingungen.</a></p>
                 </div>
             </div>
         </div>
