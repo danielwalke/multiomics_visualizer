15e4e0dbecbab2a8013fcdb99897fec8951a7caf
danielwalke <daniel.walke@ovgu.de>
2024-05-22 10:41:09 +0200

    Updates

---
+++ precache-manifest.13255d277e388cce5a07a536ff240f34.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed293dad264109d82d9e23f9856fe57",
+    "url": "/multiomics_visualizer/index.html"
+  },
+  {
+    "revision": "734aa2cf4e7908db77af",
+    "url": "/multiomics_visualizer/static/css/main.e922ad88.chunk.css"
+  },
+  {
+    "revision": "de5f40cda9d39bde0c22",
+    "url": "/multiomics_visualizer/static/js/2.8c18399e.chunk.js"
+  },
+  {
+    "revision": "a8a1a32f9d4205a1f0e7d36f16ecf67f",
+    "url": "/multiomics_visualizer/static/js/2.8c18399e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c9912abd850a74f44d",
+    "url": "/multiomics_visualizer/static/js/3.045a248b.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/multiomics_visualizer/static/js/3.045a248b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d051391c4f08b20e32",
+    "url": "/multiomics_visualizer/static/js/4.d90fe10c.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/multiomics_visualizer/static/js/4.d90fe10c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a15a0721ca87ad8bd6fd",
+    "url": "/multiomics_visualizer/static/js/5.0e91fa20.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/multiomics_visualizer/static/js/5.0e91fa20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "734aa2cf4e7908db77af",
+    "url": "/multiomics_visualizer/static/js/main.ff2a8904.chunk.js"
+  },
+  {
+    "revision": "1d18223e4c3fa48e964a",
+    "url": "/multiomics_visualizer/static/js/runtime-main.04466717.js"
+  },
+  {
+    "revision": "34684188d6782e59046176543b3e597f",
+    "url": "/multiomics_visualizer/static/media/ISAS_Logo_Standard.34684188.svg"
+  },
+  {
+    "revision": "7964c4d85c08793a04baa113133e614e",
+    "url": "/multiomics_visualizer/static/media/Sample.7964c4d8.zip"
+  },
+  {
+    "revision": "bf40c138d0bb2e1d4739239a09e8e794",
+    "url": "/multiomics_visualizer/static/media/User guide.bf40c138.pdf"
+  },
+  {
+    "revision": "7571d61622689ab45a2d5b17dfc45a96",
+    "url": "/multiomics_visualizer/static/media/disclaimer.7571d616.svg"
+  },
+  {
+    "revision": "d0cdf73f0019357bded0911bec6f7716",
+    "url": "/multiomics_visualizer/static/media/documentation.d0cdf73f.svg"
+  },
+  {
+    "revision": "c44ced4fe7a870d442d2aa737ecbcfd9",
+    "url": "/multiomics_visualizer/static/media/goal.c44ced4f.svg"
+  },
+  {
+    "revision": "ed2d7f4786f779766b6ed022940b3d71",
+    "url": "/multiomics_visualizer/static/media/note.ed2d7f47.svg"
+  }
+]);
